f94dc289791960243af82e721de55c9dea1563ab
Vishal Biradar <vishalb.engg@gmail.com>
2025-02-04 16:54:04 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,14 +14,13 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
+            Nextjs - Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               app/page.tsx
             </code>
             .
           </li>
           <li>Save and see your changes instantly.</li>
-          <li>Let's see these changes.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
